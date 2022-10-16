8cb7ab097adf7728e0a78098d7404e1488dba342
PMUCoder <pabloulled@hotmail.com>
2022-10-16 03:17:25 -0300

    toolate

--- js/login.js
+++ js/login.js
@@ -1,5 +1,57 @@
-const user="usuario1"
-const password="pass123"
+/* Objetos */
+class platformUser {
+    constructor(user,password){
+        this.user=user
+        this.password=password
+        this.id=-1
+        this.login = false
+        this.info = function(){alert("Usuario: "&user&"\nPassword: "&password)}
+    }
+}
+
+/* Array de usuarios */
+const allUsers = []
+
+/* Usuario administrador */
+user1 = new platformUser ("admin","admin")
+allUsers.push(user1.user)
+
+function startLoginProcess (){
+    let message = "Bienvendio. Seleccione la accion que desea realizar: "
+    message +=    "\n1) Tengo usuario, quiero logearme"
+    message +=    "\n2) No tengo usuario, deseo crear uno"
+    message +=    "\n3) Modificar mis datos"
+    message +=    "\n4) Eliminar mi usuario"
+    message +=    "\n5) Salir"
+
+let response = prompt(message)
+
+    switch (response){
+
+        case "1" : 
+                userLogin()
+                break
+        case "2" :          
+                createUser()
+                break
+        case "3" :
+                modifyUser()
+                break
+        case "4"  : 
+                deleteUser()
+                break
+        case "5" : 
+                alert("Va a salir de la pagina, lo esperamos nuevamente");
+                flag=false;
+                break;        
+        case null : 
+                alert("Gracias por utilizar nuestra pagina :) ");
+                flag=false;
+                break;
+        default : 
+                alert ("No ingreso una opcion valida") ;                     
+        }
+}
 
 function userLogin(){
 
@@ -38,7 +90,7 @@ function userLogin(){
 
 //support - user_login//
 function validateLogin(inputUser,inputPassword){
-    if(inputUser === user && inputPassword === password){
+    if(allUsers.includes(inputUser) && inputUser === user && inputPassword === password){
         return true
     }
     else{
@@ -46,7 +98,7 @@ function validateLogin(inputUser,inputPassword){
     }
 }
 
-// acciona los prompts
+// acciona los eventos
 document.addEventListener('DOMContentLoaded', function() {
-    userLogin()
+    startLoginProcess()
 })
