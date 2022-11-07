ae09d81b4afa66ef4a78685049355f0ebb62b50d
PMUCoder <pabloulled@hotmail.com>
2022-11-06 22:16:52 -0300

    final_WIP2

--- js/login.js
+++ js/login.js
@@ -1,81 +1,73 @@
 // create object and basic users
-
 class Login {
-    constructor (username,password,id,login_status){
+    constructor (username,password,login_status){
         this.username=username
         this.password=password
-        this.id=parseInt(id)
         this.login_status=login_status
     }
 }
 
-const admin = new Login ("admin","admin",1,false)
-const testUser = new Login ("testUser","12345",2,false)
+const admin = new Login ("admin","admin",false)
+const testUser = new Login ("testUser","12345",false)
 
 let users = []
-let usersName = []
 
 users.push(admin)
 users.push(testUser)
-usersName.push(admin.username)
-usersName.push(testUser.username)
 
-recoverUsers ()
 storeUsers ()
 
-//funcion login
-const usernameInput=document.getElementById ('username')
-const passwordInput=document.getElementById ('password')
-const loginForm = document.getElementById('loginForm')
-
-loginForm.addEventListener("submit",(e) => {
-e.preventDefault()
-
 let loginAttempts=1
-let loginAttemptsLeft=3
-loginMessage.innerText = "Ok"
+let loginAttemptsLimit=3
 
-    do{
-        loginAttemptsLeft=loginAttemptsLeft-1
-        if(validateLogin(usernameInput,passwordInput)){
-            
-            loginMessage.innerText = "El usuario se logeo con exito"
-        }
-        else if(loginAttempts < 3 && loginAttempts >= 1){
-            
-            if(loginAttemptsLeft==1){
-                warningAttempts= "Le queda 1 intento"
-                loginMessage.innerText = "Le queda 1 intento"
+btnLogin.addEventListener("click", ()=>{
+    swal.fire({
+        title: "Login",
+        html: `<input type="text" id="username" class="swal2-input" placeholder="username">
+        <input type="password" id="password" class="swal2-input" placeholder="password">`,
+        confirmButtonText: "Enviar",
+        showCancelButton: true,
+        cancelButtonText: "Cancelar",
+    }).then((result) => {
+        if(result.isConfirmed) {
+            const usernameInput=document.getElementById("username")
+            const passwordInput=document.getElementById("password")
+            let userFound = users.find(user=>user.username===usernameInput.value && user.password===passwordInput.value)
+            if(userFound){
+                swal.fire({
+                    title: "Bienvenido " +usernameInput.value,
+                    icon: "success",
+                    confirmButtonText: "Aceptar",
+                })
+                loginMessage.innerText= "El usuario "+usernameInput.value+" se logeo con exito"
+                loginAttempts=1
             }
-            else{warningAttempts="Le quedan "+loginAttemptsLeft+" intentos"
-            } 
-            loginMessage.innerText ="Los datos ingresados son incorrectos, revise usuario y contraseña e intentelo nuevamente."+"\n"+"Recuerde que luego de 3 intentos su usuario sera bloqueado por seguridad."+"\n"+warningAttempts
-            
-            loginAttempts=loginAttempts+1
-            
-        }
-        else{loginMessage.innerText ="Alcanzo el numero maximo de intentos su usuario ha sigo bloqueado, por favor contactese con nuestro centro de ayuda"
-            break
-        }        
-    }
-    while(loginAttempts <= 3)
+            else{
+                if(loginAttempts >= loginAttemptsLimit){
+                    loginMessageTxt = "Alcanzo el numero maximo de intentos su usuario ha sigo bloqueado, por favor contactese con nuestro centro de ayuda"
+                }
+                else{
+                    loginMessageTxt = "Los datos ingresados son incorrectos, revise usuario y contraseña e intentelo nuevamente."+"\n"+"Recuerde que luego de 3 intentos su usuario sera bloqueado por seguridad."+"\n"+"Le "+(loginAttempts>1 ? ("queda ") : ("quedan "))+parseInt(loginAttemptsLimit-loginAttempts)+ (loginAttempts>1 ? (" intento.") : (" intentos."))
+                }
+                    swal.fire({
+                    title: loginMessageTxt,
+                    icon: "error",
+                    confirmButtonText: "Aceptar",
+                })
+                loginMessage.innerText= loginMessageTxt
+                loginAttempts++
+            }
+        }   
+    })
+}) 
 
+function storeUsers (){
+localStorage.setItem("users",JSON.stringify(users))
+}
 
-//loginForm.reset()
-
-})
-
-//support - user_login//
-function validateLogin(usernameInput,passwordInput){
-
-    // no esta funcionando
-    if(usersName.includes(usernameInput) && users.some(passwordInput.value === password.value)){
-        alert("includes")
-        return true
-    }
-    else{
-        alert("no coincide el user o password")
-        return false
+function recoverUsers (){
+    if (localStorage.getItem("users")) {
+        users=JSON.parse(localStorage.getItem("users"))
     }
 }
 
@@ -101,12 +93,24 @@ console.log(`Element at index ${i} is ${salad[i]}`);
 //funcion logoff
 
 
-function storeUsers (){
-localStorage.setItem("users",JSON.stringify(users))
-}
 
-function recoverUsers (){
-    if (localStorage.getItem("users")) {
-        users=JSON.parse(localStorage.getItem("users"))
-    }
-}
+
+// const loginForm = document.getElementById("loginForm")
+// const usernameInput=document.getElementById ("username")
+// const passwordInput=document.getElementById ("password")
+
+//funcion login
+//loginMessage.innerText = "Ok"
+
+// loginForm.addEventListener("submit",(e) => {
+//     e.preventDefault()
+//     let userFound = users.find(user=>user.username===usernameInput.value && user.password===passwordInput.value)
+//     if(userFound){
+//         loginMessage.innerText = "El usuario se logeo con exito"
+//     }
+//     else{
+//         loginAttempts >= loginAttemptsLimit ? loginMessage.innerText = "Alcanzo el numero maximo de intentos su usuario ha sigo bloqueado, por favor contactese con nuestro centro de ayuda" : loginMessage.innerText = "Los datos ingresados son incorrectos, revise usuario y contraseña e intentelo nuevamente."+"\n"+"Recuerde que luego de 3 intentos su usuario sera bloqueado por seguridad. Le quedan "+"\n"+parseInt(loginAttemptsLimit-loginAttempts)+" intentos."
+//         loginAttempts++
+//     }
+// })
+// btnLogin.click
